9789549b8e1c013320ce30cbf20e5003eb1f4d14
olgalaskul <lashkul@skytracking.ru>
2019-03-02 17:37:47 +0300

    add express

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const http = require('http');
-const {handler} = require('./routes');
+const express = require('express');
 
-const server = http.createServer( handler);
+const app = express();
+
+//use - add new middleware
+app.use((req, res, next) => {
+  console.log('in the middleware');
+
+  //allows the continue to the next middleware
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('in the another middleware');
+
+  return res.end();
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
