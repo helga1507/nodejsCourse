d9edaa93481ceebefc59746dca84cec16414ddb1
olgalaskul <lashkul@skytracking.ru>
2019-03-03 16:13:52 +0300

    start express

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require('http');
-const {handler} = require('./routes');
+const express = require('express');
 
-const server = http.createServer( handler);
+const app = express();
 
-server.listen(3000);
+// app.use((req, res, next) => {
+//   console.log('Fist middleware');
+//
+//   next();
+// });
+//
+// app.use((req, res, next) => {
+//   console.log('Second middleware');
+//
+//   res.send('<h2>Test middleware</h2>');
+// });
+
+app.use('/users', (req, res) => {
+  res.send('<h1>Users</h1>');
+});
+
+app.use('/', (req, res) => {
+  res.send('<h1>Homepage</h1>');
+});
+
+app.listen(3000);
