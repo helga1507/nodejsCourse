0eb4e01da198612a15036e9a1ed094b33dc21463
olgalaskul <lashkul@skytracking.ru>
2019-03-05 00:26:49 +0300

    done

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const http = require('http');
-const {handler} = require('./routes');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mainRouter = require('./routes/main');
+const usersRouter = require('./routes/users');
 
-const server = http.createServer( handler);
+const app = express();
 
-server.listen(3000);
+// for request body
+app.use(bodyParser.urlencoded({extended: false}));
+
+//for public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', usersRouter);
+app.use(mainRouter);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
